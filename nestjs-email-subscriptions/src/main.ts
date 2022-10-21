d7d7de5124f2f7d47cf4687c8ca3361e0d8aa17a
Sebastian <sebastian.sloniec@gmail.com>
2022-10-21 10:26:49 +0200

    email-subscriptions msvc: finish setup

--- nestjs-email-subscriptions/src/main.ts
+++ nestjs-email-subscriptions/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  // await app.listen(3000);
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: configService.get('PORT'),
+    },
+  });
+
+  app.startAllMicroservices();
 }
 bootstrap();
--- nestjs-email-subscriptions/src/subscribers/subscribers.module.ts
+++ nestjs-email-subscriptions/src/subscribers/subscribers.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubscribersService } from './subscribers.service';
 
 import Subscriber from './subscriber.entity';
+import { SubscribersController } from './subscribers.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Subscriber])],
-  controllers: [],
   providers: [SubscribersService],
-  exports: [SubscribersService],
+  exports: [],
+  controllers: [SubscribersController],
 })
 export class SubscribersModule {}
